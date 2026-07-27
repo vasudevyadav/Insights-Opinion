26173de0b4d12b1346cca32ccd9d35409874fed7
dentistcharan <dentist.charan@gmail.com>
2026-07-27 10:54:35 +0530

    new chnages add

--- app/components/home/footer.tsx
+++ app/components/home/footer.tsx
@@ -185,7 +185,7 @@ export default function Footer() {
             <div className="mt-10 bg-[#60d4d0]">
                 <div className="mx-auto flex flex-col gap-3 px-5 py-4 text-[13px] font-medium text-white/90 sm:px-8 lg:text-base lg:flex-row lg:items-center lg:justify-between lg:px-14">
                     <p className="text-left lg:w-6/12 lg:text-base text-[13px] ">
-                        Copyright © Inshight Opinion. All rights received 2026
+                       Copyright © Insights Opinion. All rights reserved 2026.
                     </p>
 
                     <div className="flex lg:flex-row lg:w-6/12 flex-wrap items-center gap-2 lg:text-left text-center justify-center lg:justify-end">
